b8dfb645ba5d03690c0b0afa40d8e02450c822e4
Krutarth Darji <krutarth.darji@tigeranalytics.local>
2022-09-07 19:05:06 +0530

    USER Page

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import UserList from '../components/UserList'
 
 const User = () => {
-  return <h2>User works</h2>
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Dummy One',
+      image:
+        'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg',
+      places: 3,
+    },
+  ]
+  return <UserList items={USERS} />
 }
 
 export default User
