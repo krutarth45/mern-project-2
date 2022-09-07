c257fecac2b986c38a45123627946a8070851945
Krutarth Darji <krutarth.darji@tigeranalytics.local>
2022-09-07 17:54:24 +0530

    initial commit

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  endOfLine: 'auto',
+  semi: false,
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom'
+import NewPlace from './places/pages/NewPlace'
+import User from './user/pages/Users'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <User />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
