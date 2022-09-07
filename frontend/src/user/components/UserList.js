25b6c74e31b77c88d26606e558fcb15a463caf50
Krutarth Darji <krutarth.darji@tigeranalytics.local>
2022-09-07 23:18:27 +0530

    Created nav folder

--- frontend/src/user/components/UserList.js
+++ frontend/src/user/components/UserList.js
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import Card from '../../shared/components/UIElements/Card'
 import UserItem from './UserItem'
 import './UserList.css'
 
@@ -6,7 +8,9 @@ const UserList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     )
   }
