231e8871e400255359d4f3d878adc688bfec432d
Krutarth Darji <krutarth.darji@tigeranalytics.local>
2022-09-08 15:01:47 +0530

    real lecture 49 stuff

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -3,8 +3,7 @@ import { ReactDOM } from 'react-dom'
 
 import './SideDrawer.css'
 const SideDrawer = (props) => {
-  const content = <aside className="side-drawer">{props.children}</aside>
-  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
+  return <aside className="side-drawer">{props.children}</aside>
 }
 
 export default SideDrawer
