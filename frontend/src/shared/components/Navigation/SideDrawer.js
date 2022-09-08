ee91bc899f409ebf8ef6bbcb288b2b6c5a0a8179
Krutarth Darji <krutarth.darji@tigeranalytics.local>
2022-09-08 13:04:24 +0530

    Adding props in sidedrawer

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ReactDOM } from 'react-dom'
 
 import './SideDrawer.css'
-const SideDrawer = () => {
+const SideDrawer = (props) => {
   const content = <aside className="side-drawer">{props.children}</aside>
   return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
 }
