111595fd530ac6ee676d1ac4ff69ed0cbce3257a
Krutarth Darji <krutarth.darji@tigeranalytics.local>
2022-09-08 15:17:34 +0530

    handler practise

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -8,22 +8,25 @@ import SideDrawer from './SideDrawer'
 import Backdrop from '../UIElements/Backdrop'
 const MainNavigation = (props) => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false)
-  const openDrawer = () => {
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true)
   }
-  const closeDrawer = () => {
+  const closeDrawerHandler = () => {
     setDrawerIsOpen(false)
   }
   return (
     <React.Fragment>
-      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
-      <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer_nav">
           <NavLinks />
         </nav>
       </SideDrawer>
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
